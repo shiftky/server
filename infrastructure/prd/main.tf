e8be56ceb5d8883fbc2d6e836ee6b46821501e3e
shiftky <shiftky@gmail.com>
2017-08-30 06:30:01 +0000

    Setup S3 remote config

--- infrastructure/prd/main.tf
+++ infrastructure/prd/main.tf
@@ -1,14 +1,8 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
   region     = "ap-northeast-1"
 }
 
-resource "aws_kms_key" "plivo" {
-  description = "plivo kms key"
-  is_enabled  = true
-}
-
+#resource "aws_kms_key" "plivo" {
+#  description = "plivo kms key"
+#  is_enabled  = true
+#}
