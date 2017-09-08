8ac7371364ce1ab55c75dca15edabb222200521d
shiftky <shiftky@gmail.com>
2017-09-08 01:45:09 +0000

    Implement mock api response

--- functions/get_alerts/main.go
+++ functions/get_alerts/main.go
@@ -6,18 +6,49 @@ import (
 	"github.com/apex/go-apex"
 )
 
-type message struct {
-	Hello string `json:"hello"`
+type Alerts struct {
+	Alerts []Alert `json:"alerts"`
+}
+
+type Alert struct {
+	Id        string `json:"id"`
+	OrgName   string `json:"orgName"`
+	Title     string `json:"title"`
+	Status    string `json:"status"`
+	IsOpen    bool   `json:"isOpen"`
+	Url       string `json:"url"`
+	Trigger   string `json:"trigger"`
+	CreatedAt string `json:"createdAt"`
 }
 
 func main() {
 	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-		var m message
+		var alerts []Alert
 
-		if err := json.Unmarshal(event, &m); err != nil {
-			return nil, err
-		}
+		alerts = append(alerts, Alert{
+			Id:        "35UYbW8t1j5",
+			OrgName:   "gazou",
+			Title:     "prd-db/check_load: CheckLoad CRITICAL: Per core load average (12 CPU): [3.58, 3.49, 2.31]",
+			Status:    "critical",
+			IsOpen:    true,
+			Url:       "https://example.com/alert",
+			Trigger:   "monitor",
+			CreatedAt: "2017-09-07 18:41:44 +0900",
+		})
 
-		return m, nil
+		alerts = append(alerts, Alert{
+			Id:        "123ABCDEFG",
+			OrgName:   "gazou",
+			Title:     "prd-db/check_load: CheckLoad OK: Per core load average (12 CPU): [0.08, 1.29, 1.90]",
+			Status:    "ok",
+			IsOpen:    false,
+			Url:       "https://example.com/alert",
+			Trigger:   "monitor",
+			CreatedAt: "2017-09-07 20:30:00 +0900",
+		})
+
+		return Alerts{
+			Alerts: alerts,
+		}, nil
 	})
 }
