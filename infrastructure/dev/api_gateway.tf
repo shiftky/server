11be7b0b654138ae44a6fa6a687e6174a32d4be7
shiftky <shiftky@gmail.com>
2017-09-11 07:48:51 +0000

    Modify API resource structure

--- infrastructure/dev/api_gateway.tf
+++ infrastructure/dev/api_gateway.tf
@@ -1,11 +1,3 @@
-variable "aws_region" {
-  type = "string"
-}
-
-variable "apex_function_arns" {
-  type = "map"
-}
-
 resource "aws_api_gateway_rest_api" "incident-app-team-a" {
   name = "incident-app-team-a"
 }
@@ -13,7 +5,6 @@ resource "aws_api_gateway_rest_api" "incident-app-team-a" {
 #
 # /alerts
 #
-
 ## resource
 resource "aws_api_gateway_resource" "alerts" {
   rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
@@ -29,28 +20,11 @@ resource "aws_api_gateway_method" "get_alerts" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "post_alerts" {
-  rest_api_id   = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  resource_id   = "${aws_api_gateway_resource.alerts.id}"
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
 ## method response
 resource "aws_api_gateway_method_response" "get_alerts_200" {
   rest_api_id     = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
   resource_id     = "${aws_api_gateway_resource.alerts.id}"
   http_method     = "${aws_api_gateway_method.get_alerts.http_method}"
-  response_models = {
-    "application/json" = "Empty"
-  }
-  status_code     = "200"
-}
-
-resource "aws_api_gateway_method_response" "post_alerts_200" {
-  rest_api_id     = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  resource_id     = "${aws_api_gateway_resource.alerts.id}"
-  http_method     = "${aws_api_gateway_method.post_alerts.http_method}"
   response_models = {
     "application/json" = "Empty"
   }
@@ -68,15 +42,6 @@ resource "aws_api_gateway_integration" "get_alerts" {
   uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${lookup(var.apex_function_arns, "get_alerts")}/invocations"
 }
 
-resource "aws_api_gateway_integration" "post_alerts" {
-  rest_api_id             = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  resource_id             = "${aws_api_gateway_resource.alerts.id}"
-  http_method             = "${aws_api_gateway_method.post_alerts.http_method}"
-  type                    = "AWS"
-  integration_http_method = "POST"
-  content_handling        = "CONVERT_TO_TEXT"
-  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${lookup(var.apex_function_arns, "post_alerts")}/invocations"
-}
 
 ## integration response
 resource "aws_api_gateway_integration_response" "get_alerts_200" {
@@ -86,22 +51,64 @@ resource "aws_api_gateway_integration_response" "get_alerts_200" {
   status_code = "${aws_api_gateway_method_response.get_alerts_200.status_code}"
 }
 
+
+
+#
+# /alerts/mackerel
+#
+## resource
+resource "aws_api_gateway_resource" "alerts_mackerel" {
+  rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
+  parent_id = "${aws_api_gateway_resource.alerts.id}"
+  path_part   = "mackerel"
+}
+
+## method
+resource "aws_api_gateway_method" "post_alerts_mackerel" {
+  rest_api_id   = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
+  resource_id   = "${aws_api_gateway_resource.alerts_mackerel.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+## method response
+resource "aws_api_gateway_method_response" "post_alerts_mackerel_200" {
+  rest_api_id     = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
+  resource_id   = "${aws_api_gateway_resource.alerts_mackerel.id}"
+  http_method     = "${aws_api_gateway_method.post_alerts_mackerel.http_method}"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  status_code     = "200"
+}
+
+## integration
+resource "aws_api_gateway_integration" "post_alerts_mackerel" {
+  rest_api_id             = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
+  resource_id             = "${aws_api_gateway_resource.alerts_mackerel.id}"
+  http_method             = "${aws_api_gateway_method.post_alerts_mackerel.http_method}"
+  type                    = "AWS"
+  integration_http_method = "POST"
+  content_handling        = "CONVERT_TO_TEXT"
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${lookup(var.apex_function_arns, "post_alerts")}/invocations"
+}
+
+## integration response
 resource "aws_api_gateway_integration_response" "post_alerts_200" {
   rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  resource_id = "${aws_api_gateway_resource.alerts.id}"
-  http_method = "${aws_api_gateway_method.post_alerts.http_method}"
-  status_code = "${aws_api_gateway_method_response.post_alerts_200.status_code}"
+  resource_id = "${aws_api_gateway_resource.alerts_mackerel.id}"
+  http_method = "${aws_api_gateway_method.post_alerts_mackerel.http_method}"
+  status_code = "${aws_api_gateway_method_response.post_alerts_mackerel_200.status_code}"
 }
 
 
 #
 # deployment
 #
-
 resource "aws_api_gateway_deployment" "incident-app-team-a" {
   depends_on  = [
     "aws_api_gateway_method.get_alerts",
-    "aws_api_gateway_method.post_alerts"
+    "aws_api_gateway_method.post_alerts_mackerel"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
   stage_name  = "development"
---
+++ infrastructure/dev/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  type = "string"
+}
+
+variable "apex_function_arns" {
+  type = "map"
+}
