746003299fbb64a10e8c6f316a392e078f38bae5
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-21 19:09:27 +0900

    Remove api_gateway_deployment resource

--- infrastructure/dev/api_gateway.tf
+++ infrastructure/dev/api_gateway.tf
@@ -4,19 +4,6 @@ resource "aws_api_gateway_rest_api" "incident-app-team-a" {
   name = "incident-app-team-a"
 }
 
-#
-# deployment
-#
-resource "aws_api_gateway_deployment" "incident-app-team-a" {
-  depends_on = [
-    "aws_api_gateway_method.get_alerts",
-    "aws_api_gateway_method.post_alerts_mackerel",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  stage_name  = "development"
-}
-
 resource "aws_iam_role_policy_attachment" "dynamodb-lambda" {
   role       = "${element(split("/",var.apex_function_role), 1)}"
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
