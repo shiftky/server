cc70176056fedee61f1ab249f83555ac6b8c08de
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-21 17:56:27 +0900

    Remove aws_api_gateway_deployment resource for use tf_aws_apigateway_apex module

--- infrastructure/dev/api_gateway.tf
+++ infrastructure/dev/api_gateway.tf
@@ -3,16 +3,3 @@ data "aws_caller_identity" "current" {}
 resource "aws_api_gateway_rest_api" "incident-app-team-a" {
   name = "incident-app-team-a"
 }
-
-#
-# deployment
-#
-resource "aws_api_gateway_deployment" "incident-app-team-a" {
-  depends_on = [
-    "aws_api_gateway_method.get_alerts",
-    "aws_api_gateway_method.post_alerts_mackerel",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
-  stage_name  = "development"
-}
