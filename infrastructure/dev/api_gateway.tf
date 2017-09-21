50b691d15e7bffc7ac5fc59b4846182d4040f7f5
epy0n0ff <epy0n.0ff@gmail.com>
2017-09-21 18:51:28 +0900

    Add dynamodb policy to apex function role

--- infrastructure/dev/api_gateway.tf
+++ infrastructure/dev/api_gateway.tf
@@ -16,3 +16,8 @@ resource "aws_api_gateway_deployment" "incident-app-team-a" {
   rest_api_id = "${aws_api_gateway_rest_api.incident-app-team-a.id}"
   stage_name  = "development"
 }
+
+resource "aws_iam_role_policy_attachment" "dynamodb-lambda" {
+  role       = "${element(split("/",var.apex_function_role), 1)}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
